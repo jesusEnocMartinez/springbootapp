36f0d4245c53b41c8d1ca5e24b43c0ba70ee20c8
jesusenoc <jesusenoc1234@gmail.com>
2020-12-21 22:49:11 -0600

    entidades

---
+++ src/main/java/org/enoc/uv/EnocApplication.java
@@ -0,0 +1,13 @@
+package org.enoc.uv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnocApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/enoc/uv/entity/User.java
@@ -0,0 +1,123 @@
+package org.enoc.uv.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Set;
+
+public class User implements Serializable {
+
+    private static final long serialVersionUID = -6833167247955613395L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO, generator="native")
+    @GenericGenerator(name="native",strategy="native")
+    private Long id;
+
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+    @Column(unique = true)
+    private String email;
+    @Column(unique = true)
+    private String username;
+    @Column
+    private String password;
+
+    @Transient
+    private String confirmPassword;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name="user_roles"
+            ,joinColumns=@JoinColumn(name="user_id")
+            ,inverseJoinColumns=@JoinColumn(name="role_id"))
+    private Set roles;
+
+    public User() {	}
+
+    public User(Long id) {
+        this.id = id;
+    }
+
+    public User(Long id, String firstName, String lastName, String email, String username, String password, String confirmPassword, Set roles) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+        this.roles = roles;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public Set getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set roles) {
+        this.roles = roles;
+    }
+}
